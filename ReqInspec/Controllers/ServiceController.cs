66385a3e7bcf56d0e29add2afcc30e3ca58b05f4
andreij6 <andreij6@gmail.com>
2014-10-01 11:00:05 -0400

    result chng

--- ReqInspec/Controllers/ServiceController.cs
+++ ReqInspec/Controllers/ServiceController.cs
@@ -17,13 +17,22 @@ namespace ReqInspec.Controllers
         public string VerifyAddress()
         {
             string result =
-                    @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"">" +
-                        "<soapenv:Body>" +
-                            @"<notificationsResponse xmlns=""http://soap.sforce.com/2005/09/outbound"">" +
-                                "<Ack>true</Ack>" +
-                            "</notificationsResponse>" +
-                        "</soapenv:Body>" +
-                    "</soapenv:Envelope>";
+                        @"<?xml version=""1.0"" encoding=""utf-8""?>" +
+                            @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">" +
+                                "<soapenv:Body>" +
+                                @"<notificationsResponse xmlns=""http://soap.sforce.com/2005/09/outbound"">" +
+                                    @"<Ack>true</Ack>" +
+                                "</notificationsResponse>" +
+                            "</soapenv:Body>" +
+                        "</soapenv:Envelope>";
+            //string result =
+            //        @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"">" +
+            //            "<soapenv:Body>" +
+            //                @"<notificationsResponse xmlns=""http://soap.sforce.com/2005/09/outbound"">" +
+            //                    "<Ack>true</Ack>" +
+            //                "</notificationsResponse>" +
+            //            "</soapenv:Body>" +
+            //        "</soapenv:Envelope>";
 
             return result;
         }
