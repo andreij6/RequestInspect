f3d5b6435193963441f90ca487b72f9730bcd60b
andreij6 <andreij6@gmail.com>
2014-10-01 10:46:38 -0400

    init

---
+++ ReqInspec/Controllers/ServiceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ReqInspec.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        public string VerifyAddress()
+        {
+            string result =
+                    @"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"">" +
+                        "<soapenv:Body>" +
+                            @"<notificationsResponse xmlns=""http://soap.sforce.com/2005/09/outbound"">" +
+                                "<Ack>true</Ack>" +
+                            "</notificationsResponse>" +
+                        "</soapenv:Body>" +
+                    "</soapenv:Envelope>";
+
+            return result;
+        }
+    }
+}
